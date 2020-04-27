18a2941e5ab3058c53564249892556c189a1a8f7
Stephen Peters <7speter@gmail.com>
2020-04-27 18:06:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times {puts message}
+end
+
+
+def loop_message_n_times
+
+end
+
+
+def output_array
+
+end
+
+
+def return_string_array
+
+end
